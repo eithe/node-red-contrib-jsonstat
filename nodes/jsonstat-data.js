6666a170e112652951dbaf0dfd911b96d6603ee5
Eirik Hodne <eirik.hodne@geodata.no>
2019-01-09 13:37:24 +0100

    License

--- nodes/jsonstat-data.js
+++ nodes/jsonstat-data.js
@@ -1,6 +1,6 @@
 /**
  * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 43):
+ * "THE BEER-WARE LICENSE" (Revision 42):
  * <post  eirikh no> wrote this file.  As long as you retain this notice you
  * can do whatever you want with my stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return.   Eirik H
